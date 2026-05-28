ab362fe9f820797efbd1728907f4ffc0c0248223
sadhu.sreenivas <sadhu.sreenivas@gmail.com>
2026-05-28 18:06:11 +0530

    fix: make deploy builds node20 compatible

--- middleware/netlify-db-service/.netlify/functions/admin.js
+++ middleware/netlify-db-service/.netlify/functions/admin.js
@@ -13,14 +13,31 @@ dotenv.config();
 process.env.ADMIN_SERVICE_LISTEN = 'false';
 
 const serverless = serverlessModule.default || serverlessModule;
-const appModule = await import('../../../admin-service/src/server.js');
-const app = appModule.default || appModule;
 
-const handler = serverless(app, {
-  binary: ['text/csv'],
-  request: () => {
-    if (!process.env.NETLIFY) process.env.NETLIFY = 'true';
-  }
-});
+let cachedHandler;
+
+async function getHandler() {
+  if (cachedHandler) return cachedHandler;
+
+  if (!process.env.NETLIFY) process.env.NETLIFY = 'true';
+  process.env.ADMIN_SERVICE_LISTEN = 'false';
+
+  const appModule = await import('../../../admin-service/src/server.js');
+  const app = appModule.default || appModule;
+
+  cachedHandler = serverless(app, {
+    binary: ['text/csv'],
+    request: () => {
+      if (!process.env.NETLIFY) process.env.NETLIFY = 'true';
+    }
+  });
+
+  return cachedHandler;
+}
+
+const handler = async (event, context) => {
+  const resolvedHandler = await getHandler();
+  return resolvedHandler(event, context);
+};
 
 export { handler };
